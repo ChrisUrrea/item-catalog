fb796a192292cac6d47e575427cda26a99bfc496
Chrisaric <chrisaric@github.com>
2019-02-13 11:27:59 -0500

    ctalog

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,58 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+import os, sys
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class PawnShop(Base):
+    __tablename__ = 'pawnshop'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'name': self.name,
+            'id': self.id
+        }
+
+class Valuable(Base):
+    __tablename__ = 'valuable'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    pawnshop_id = Column(Integer, ForeignKey('pawnshop.id'))
+    pawnshop = relationship(PawnShop)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price
+        }
+
+engine = create_engine('sqlite:///pawnshopvaluables.db')
+
+Base.metadata.create_all(engine)
